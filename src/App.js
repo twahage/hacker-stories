3602ba08bf87398aa3fcf4d7d2d99fbd0f6c6c8f
Tamim Wahage <twahage@gmail.com>
2021-09-01 16:43:55 +0200

    INIT

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function getTitle(title) {
+  return title;
+}
+
+function App() {
+  //do something in between
+
+  return (
+    <div>
+      <h1>
+        Hello {getTitle('React')}
+      </h1>  
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
