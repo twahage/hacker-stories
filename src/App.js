98128e4e6056dc278c302c85955aaab4a3e76741
Tamim Wahage <twahage@gmail.com>
2021-11-11 10:47:13 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-];
+
 
 const App = () => {
+
+    const stories = [
+      {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+      },
+      {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+      },
+    ];
+
     const handleChange = event => {
-      console.log(event);
+      console.log(event.target.value);
     };
 
     return (
@@ -36,13 +39,13 @@ const App = () => {
 
         <hr />
 
-        <List />
+        <List list={stories} />
       </div>
     )
 };
 
-const List = () => 
-  list.map(item => (
+const List = props => 
+  props.list.map(item => (
       <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
